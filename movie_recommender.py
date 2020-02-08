4a7c0b74ef356a7a08be77613836b974a44b6604
danishsshaikh <danishsshaikh@gmail.com>
2020-02-09 00:05:51 +0530

    Update movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -59,5 +59,7 @@ similar_movies =  list(enumerate(cosine_sim[movie_index]))
 
 ## Step 7: Get a list of similar movies in descending order of similarity score
 
+sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)
+
 
 ## Step 8: Print titles of first 50 movies
